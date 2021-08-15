00a33f62ecfeae50c61f62ece66b37a816540b81
battleplanweb <info@battleplanwebdesign.com>
2021-08-15 06:42:52 -0400

    Initial commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'bb5fa018296ae7f569077165745d23a6b7b388c8',
+        'name' => 'yoast/wordpress-seo-local',
+        'dev' => false,
+    ),
+    'versions' => array(
+        'yoast/i18n-module' => array(
+            'pretty_version' => '3.1.1',
+            'version' => '3.1.1.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../yoast/i18n-module',
+            'aliases' => array(),
+            'reference' => '9d0a2f6daea6fb42376b023e7778294d19edd85d',
+            'dev_requirement' => false,
+        ),
+        'yoast/wordpress-seo-local' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'bb5fa018296ae7f569077165745d23a6b7b388c8',
+            'dev_requirement' => false,
+        ),
+    ),
+);
